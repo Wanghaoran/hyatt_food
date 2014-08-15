193358507188e0f640211c494d429eba9a428203
WangHaoRan <whrlmc@139.com>
2014-08-15 16:06:25 +0800

    22

--- application/views/index.php
+++ application/views/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" style="background-color: #ffffff;">
 <html xmlns:wb="http://open.weibo.com/wb">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -17,7 +17,7 @@
     </script>
 
 </head>
-<body>
+<body >
 <div class="contain">
 <!--头部 start-->
 <div class="header">
