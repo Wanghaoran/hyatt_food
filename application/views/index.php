a370e3e9e4125ac9f9c884008447744473632ac3
WangHaoRan <whrlmc@139.com>
2014-08-27 10:59:24 +0800

    333

--- application/views/index.php
+++ application/views/index.php
@@ -110,7 +110,8 @@
         <div class="pop_input"><input id="kw" name="keyword" value="请输入您的邮箱" onfocus="this.value='';this.style.color='#333'" onblur="if(this.value==''){this.value='请输入您的邮箱';this.style.color='#8b8b8b'}">
         </div>
         <div class="clear20"></div>
-        <div class="pop_join"><a href="javascript:void(0);" onclick="alert('活动将于9月1日正式上线，请您届时加入凯悦悦享家！');" title="点击加入“凯悦悦享家”"></a></div>
+        <div class="pop_join"><a href="javascript:void(0);" onclick="alert('活动将于9月1日正式上线，请您届时加入凯悦悦享家！');" title="点击加入“凯悦悦享家”"></a><a href="javascript:closeBg(11)" class="tiaoguo">跳过</a></div>
+
     </div>
 </div>
 <div id="hidebg" class="hidebg" style="display:none"></div>
