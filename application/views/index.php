75f75d577e6ed67ec96a847a52c4093715c96f3c
WangHaoRan <whrlmc@139.com>
2014-08-31 12:40:34 +0800

    222

--- application/views/index.php
+++ application/views/index.php
@@ -78,7 +78,7 @@
 
 
         var openshare = function(){
-            window.open (url, '分享到新浪微博', 'height=150, width=400, top=0, left=0, toolbar=no, menubar=no, scrollbars=yes, resizable=no,location=n o, status=no');
+            window.open (url, '分享到新浪微博', 'height=200, width=400, top=0, left=0, toolbar=no, menubar=no, scrollbars=yes, resizable=no,location=n o, status=no');
         }
 
 
