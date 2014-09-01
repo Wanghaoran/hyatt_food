f044010744bd830badd26740c662318332879f9b
WangHaoRan <whrlmc@139.com>
2014-09-01 11:31:05 +0800

    2323

--- application/views/all.php
+++ application/views/all.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7">
     <title>最佳时令菜肴投票-凯悦悦享家</title>
     <link href="<?=$this->config->base_url()?>static/css/style.css" rel="stylesheet" type="text/css" />
     <script type="text/javascript" src="<?=$this->config->base_url()?>static/javascript/jquery.js"></script>
