82d44d2496531655deefd6cfd315ffbb6c0a1ade
WangHaoRan <whrlmc@139.com>
2014-09-04 03:16:10 +0800

    333

--- application/views/mobile_index.php
+++ application/views/mobile_index.php
@@ -1,1 +1,10 @@
-<?='移动客户端页面稍后上线，敬请期待。。。'?>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+<h1><?='移动客户端页面稍后上线，敬请期待。。。'?></h1>
+</body>
+
+</html>
