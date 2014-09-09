33aa92ef061cc1329bbbcb9e06b456e5478bc0c5
WangHaoRan <whrlmc@139.com>
2014-09-10 03:34:53 +0800

    2323

--- application/views/index_mobile.php
+++ application/views/index_mobile.php
@@ -15,10 +15,11 @@
 
     <script>
 
+        /* H5底导未开放
         WeiboJSBridge.trigger('bottomNavigation:info', {
             "title" : "我的分享文案，爱怎么写就怎么写，哈哈"
         }, function(){});
-
+        */
 
         //点击投票
         var sharebutton = function(cid){
