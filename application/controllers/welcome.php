85bb65b11a7743f7cbd80fa32e3c4d1140c8a88c
WangHaoRan <whrlmc@139.com>
2014-09-12 11:52:01 +0800

    23232

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -47,10 +47,10 @@ class Welcome extends CI_Controller {
             'show_num' => $show_num,
         );
 
-//        $this -> load -> view('index', $data);
+        $this -> load -> view('index', $data);
 
 
-
+/*
         $this -> load -> library('user_agent');
 
 
@@ -66,6 +66,7 @@ class Welcome extends CI_Controller {
             $this -> load -> view('index_mobile', $data);
         }
 
+*/
 
     }
 
