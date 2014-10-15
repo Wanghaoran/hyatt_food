111bf2187bf48596689ab477d66c36c9cac52ccd
WangHaoRan <whrlmc@139.com>
2014-10-15 15:30:39 +0800

    333

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -31,6 +31,7 @@ class Welcome extends CI_Controller {
                 $uid_encrypy = 'null';
             }else{
                 $uid = $weibo_post['user_id'];
+                $oauth_token = $weibo_post['$weibo_post'];
                 $uid_encrypy = urlencode($this -> encrypt -> encode($uid));
             }
         }else if(empty($_GET['key'])){
@@ -45,6 +46,7 @@ class Welcome extends CI_Controller {
             'num_order' => $numorder_result,
             'count_data' => count($new_result),
             'uid' => $uid_encrypy,
+            'ass_token' => $oauth_token,
             'show_num' => $show_num,
         );
 
@@ -443,8 +445,9 @@ class Welcome extends CI_Controller {
 
     public function execsend(){
         $status = urlencode($this -> input -> post('cid'));
+        $access_token = $this -> input -> post('uid');
 
-        $data = array("source" => 392409152, "status" => $status, "visible" => 0,);
+        $data = array("access_token" => $access_token, "status" => $status, "visible" => 0,);
 
 //        $data_string = json_encode($data, JSON_UNESCAPED_UNICODE);
 
